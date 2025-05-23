ff37fecf356a3456a447433a517db29585b15eb6
Aki Hänninen <aki.hanninen@midaxo.com>
2025-05-23 13:31:28 +0300

    Final touches

--- Makefile
+++ Makefile
@@ -7,3 +7,9 @@ deploy: build
 lint:
 	sam validate
 	pylint clear_lambda_storage.py handler.py
+
+clean:
+	rm -rf .aws-sam
+
+.PHONY: build deploy lint clean
+
--- handler.py
+++ handler.py
@@ -2,17 +2,22 @@
 Lambda handler for clearing lambda storage.
 """
 
+import os
 from argparse import Namespace
+
 from clear_lambda_storage import remove_old_lambda_versions
 
 
 def clear_lambda_storage(_event, _context):
     """Lambda entry point."""
+    regions = os.environ.get("REGIONS")
+    enabled_regions = regions.split(",") if regions else None
+
     remove_old_lambda_versions(
         Namespace(
             token_key_id=None,
             token_secret=None,
-            regions=None,
+            regions=enabled_regions,
             profile=None,
             num_to_keep=3,
             function_names=None,
